fa2e22513092eb34ddccb3ab357aeab289b76faa
Maria-Tkh <mcweaver117@gmail.com>
2021-10-03 00:22:32 +0300

    fetches

--- src/views/ReviewsView.jsx
+++ src/views/ReviewsView.jsx
@@ -4,11 +4,11 @@ import { fetchMovieReviews } from '../services/api';
 export const ReviewsView = ({ movieId }) => {
   const [reviews, setReviews] = useState(null);
 
-  // useEffect(() => {
-  //   fetchMovieReviews(movieId).then(response => {
-  //     setReviews(response.results);
-  //   });
-  // }, [movieId]);
+  useEffect(() => {
+    fetchMovieReviews(movieId).then(response => {
+      setReviews(response.results);
+    });
+  }, [movieId]);
 
   console.log(reviews);
 
